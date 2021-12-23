be0bafd109721dfdfe33799fa8e4636a6c4e21c8
Road <496814664@qq.com>
2021-12-23 16:22:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Config LTE network
+sed -i '$d' package/base-files/files/bin/config_generate
+sed -i 'uci set network.wwan=interface' package/base-files/files/bin/config_generate
+sed -i 'uci set network.wwan.ifname=eth2' package/base-files/files/bin/config_generate
+sed -i 'uci set network.wwan.proto=dhcp' package/base-files/files/bin/config_generate
+sed -i 'uci set network.wwan.up=1' package/base-files/files/bin/config_generate
+sed -i 'uci commit' package/base-files/files/bin/config_generate
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
