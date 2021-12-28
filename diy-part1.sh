3befa90b50b4b69c7877a1f7f34ba6c86e8d7083
Road <496814664@qq.com>
2021-12-28 11:56:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@ popd
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git luci https://github.com/Lienol/luci.git;19.07' >>feeds.conf.default
